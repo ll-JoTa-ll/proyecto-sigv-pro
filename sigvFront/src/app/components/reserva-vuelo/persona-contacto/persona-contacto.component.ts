6dece774df49269be20c0dad305b60c09be2680e
bertol26 <analista8@domiruth.com>
2019-11-13 11:02:31 -0500

    vuelos

--- sigvFront/src/app/components/reserva-vuelo/persona-contacto/persona-contacto.component.ts
+++ sigvFront/src/app/components/reserva-vuelo/persona-contacto/persona-contacto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ICostCenter } from '../../../models/ICostCenter';
+import { SessionStorageService } from 'ngx-webstorage';
 
 declare var jquery: any;
 declare var $: any;
@@ -12,14 +13,19 @@ declare var $: any;
 export class PersonaContactoComponent implements OnInit {
 
   @Input() lsCostCenter: ICostCenter[];
+  datosuser: any[] = [];
+  inderror: boolean;
 
-  constructor() { }
+  constructor(private sessionStorageService: SessionStorageService) {
+    this.datosuser = sessionStorageService.retrieve('objusuarios');
+   }
 
   ngOnInit() {
   }
 
 
   ValidarCampos() {
+    let regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
     if ($('#contactocorreo').val().length <= 0) {
       $('#contactocorreo').addClass('campo-invalido');
     } else {
@@ -30,6 +36,12 @@ export class PersonaContactoComponent implements OnInit {
       $('#contactotelefono').addClass('campo-invalido');
     } else {
       $('#contactotelefono').removeClass('campo-invalido');
+    }
+
+    if (regex.test($('#contactocorreo').val().trim())) {
+      this.inderror = false;
+    } else {
+     this.inderror = true;
     }
   }
 }
