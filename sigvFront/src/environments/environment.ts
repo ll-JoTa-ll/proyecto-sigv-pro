4def7fe0e0add87a04a966a7131fca6a280932a0
bertol26 <analista8@domiruth.com>
2019-11-13 15:45:39 -0500

    cambios vuelos

--- sigvFront/src/environments/environment.ts
+++ sigvFront/src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  url: "http://sigvplussecurity.azurewebsites.net/api/",
-  url_2: "http://sigvplusflight.azurewebsites.net/api/",
+  url: "http://apis-uat.domiruth.com/security/api/",
+  url_2: "http://apis-uat.domiruth.com/flight/api/",
  // url_hotel: "http://172.16.3.20:8084/api/",
   url_hotel: "http://sigvplushotel.azurewebsites.net/api/",
   url_usuario : "http://172.16.2.101:8110/api/",
   url_project :"http://localhost:4200",
-  url_5: 'https://sigvpluscustomer.azurewebsites.net/api/',
-  url_6: 'http://sigvplusnotification.azurewebsites.net/api/',
+  url_5: 'http://apis-uat.domiruth.com/customer/api/',
+  url_6: 'http://apis-uat.domiruth.com/notification/api/',
   cod_rol_autogestion: [3],
   cod_rol_autorizador: [4],
   cod_rol_centralizador: [5, 2],
